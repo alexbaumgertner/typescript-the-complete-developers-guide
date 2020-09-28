832a72963bfa449bcaccb857311af6f81f1effce
alexbaumgertner <alex.baumgertner@gmail.com>
2020-09-28 16:58:11 +0300

    Learn void and never types

--- functions.ts
+++ functions.ts
@@ -1,3 +1,15 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+function divide(a:number, b:number):number {
+    return a / b;
+}
+
+const logger = (message: string): void => {
+    console.log(message);
+}
+
+const throwError = (message: string): never => {
+    throw new Error(message);
+}
